9d0bee858dad8db2d49d5a6bc49ee8dac58296b6
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-04-10 17:17:11 +0300

    Fixed: task_2.rb according pipeline`s rules

--- task_2.rb
+++ task_2.rb
@@ -15,7 +15,10 @@
 def likes(names)
   case names.length
   when 0 then 'no one likes this'
-  when 1, 2, 3 then "#{names[0..names.length - 1].join(', ')} likes this"
-  else "#{names[0]}, #{names[1]} and #{names.length - 2} like this"
+  when 1 then "#{names[0]} likes this"
+  when 2 then "#{names[0]} and #{names[1]} like this"
+  when 3 then "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  else
+    "#{names[0]}, #{names[1]} and #{names.length - 2} others like this"
   end
 end
