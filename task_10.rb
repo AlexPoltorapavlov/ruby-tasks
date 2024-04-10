bbcb54311600d4f9b61831cf238bf24ee781417e
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-04-10 16:33:30 +0300

    Update: task_10.rb ```def numbers_sum(end_num)```

--- task_10.rb
+++ task_10.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 # Написать функцию, которая возращает сумму целых чисел от 1-го до заданного значения
 
 def numbers_sum(end_num)
-  # your code
+  range_sum = (1..end_num)
+  sum = 0
+  range_sum.each do |i|
+    sum += i
+  end
+  sum
 end
+
+puts(numbers_sum(3))
