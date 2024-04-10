ce13e83c19bab006ec721d4a65b5a0f044544550
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-04-10 16:36:23 +0300

    Update: task_10.rb in def numbers_sum(end_num) used math way

--- task_10.rb
+++ task_10.rb
@@ -3,12 +3,5 @@
 # Написать функцию, которая возращает сумму целых чисел от 1-го до заданного значения
 
 def numbers_sum(end_num)
-  range_sum = (1..end_num)
-  sum = 0
-  range_sum.each do |i|
-    sum += i
-  end
-  sum
+  (end_num * (end_num + 1)) / 2
 end
-
-puts(numbers_sum(3))
