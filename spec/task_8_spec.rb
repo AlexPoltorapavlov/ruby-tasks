b972a14a0859d9639b8959052b940690121ce825
Poliev Alexey <apoliev@rnds.pro>
2024-04-10 11:55:17 +0300

    update test

--- spec/task_8_spec.rb
+++ spec/task_8_spec.rb
@@ -23,5 +23,6 @@ describe 'task_8' do
     expect(sanitize('.')).to eq('\.')
     expect(sanitize('!')).to eq('\!')
     expect(sanitize('Hello, world!')).to eq('Hello\, world\!')
+    expect(sanitize('@?')).to eq('@?')
   end
 end
