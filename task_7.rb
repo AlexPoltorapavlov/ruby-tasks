f123284b328b35b3bc77e4a4a09e7bf6c4f9afcd
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-04-10 15:17:11 +0300

    Update: task_7.rb ```def domain_name(url)```

--- task_7.rb
+++ task_7.rb
@@ -1,8 +1,14 @@
-# Напишите функцию, которая при задании URL-адреса в виде строки анализирует только имя домена и возвращает его в виде строки. Например:
+# frozen_string_literal: true
+
+# Напишите функцию, которая при задании URL-адреса в виде строки анализирует только имя домена и возвращает его в виде
+# строки. Например:
 # * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
 # * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 # * url = "https://www.cnet.com"                -> domain name = cnet"
 
 def domain_name(url)
-  # your code
+  url = url.gsub('http://', '')
+  url = url.gsub('https://', '')
+  url = url.gsub('www.', '')
+  url.split('.').first
 end
