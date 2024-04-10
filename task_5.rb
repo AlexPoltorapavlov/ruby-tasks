748b5047dc814f76a32db6bc18990c4af291f027
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-04-10 14:16:22 +0300

    Update: task_5.rb ```class PaginationHelper```

--- task_5.rb
+++ task_5.rb
@@ -1,6 +1,10 @@
-# Вы завершите работу над классом PaginationHelper, который представляет собой служебный класс, полезный для запроса информации о подкачке, связанной с массивом.
+# frozen_string_literal: true
 
-# Класс предназначен для приема массива значений и целого числа, указывающего, сколько элементов будет разрешено на каждой странице.
+# Вы завершите работу над классом PaginationHelper, который представляет собой служебный класс, полезный для запроса
+# информации о подкачке, связанной с массивом.
+
+# Класс предназначен для приема массива значений и целого числа, указывающего, сколько элементов будет разрешено на
+# каждой странице.
 # Типы значений, содержащихся в коллекции/массиве, не имеют значения.
 
 # Ниже приведены некоторые примеры использования этого класса:
@@ -17,34 +21,45 @@
 # helper.page_index(20) # should == -1
 # helper.page_index(-10) # should == -1 because negative indexes are invalid
 
+# Description/Explanation of PaginationHelper class
 class PaginationHelper
   attr_reader :collection, :items_per_page
 
   # The constructor takes in an array of items and a integer indicating how many
   # items fit within a single page
   def initialize(collection, items_per_page)
-    # your code
+    @collection = collection
+    @items_per_page = items_per_page
   end
 
   # returns the number of items within the entire collection
   def item_count
-    # your code
+    collection.size
   end
 
   # returns the number of pages
   def page_count
-    # your code
+    (collection / items_per_page).ceil
   end
 
   # returns the number of items on the current page. page_index is zero based.
   # this method should return -1 for page_index values that are out of range
   def page_item_count(page_index)
-    # your code
+    if page_index.negative? || page_index >= page_count
+      -1
+    else
+      is_last_page = page_index == page_count - 1
+      is_last_page ? item_count % items_per_page : items_per_page
+    end
   end
 
   # determines what page an item is on. Zero based indexes.
   # this method should return -1 for item_index values that are out of range
   def page_index(item_index)
-    # your code
+    if item_index.negative? || item_index >= item_count
+      -1
+    else
+      item_index / items_per_page
+    end
   end
 end
