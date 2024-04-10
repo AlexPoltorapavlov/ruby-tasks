59dc0d325da3534ec800d064b92f8894b8854aaa
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-04-10 12:24:10 +0300

    Update: task_3.rb ```def filter_list(list)```

--- task_3.rb
+++ task_3.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 # Напишите функцию, которая принимает список чисел и строк и возвращает новый список с отфильтрованными строками
 # filter_list([1,2,'a','b']) == [1,2]
 # filter_list([1,'a','b',0,15]) == [1,0,15]
 # filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 def filter_list(list)
-  # your code
+  list.select { |item| item.is_ a?(Integer || Float) }
 end
